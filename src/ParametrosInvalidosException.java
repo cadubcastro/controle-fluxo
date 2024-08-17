7d454d5eda7278a9dbcfcd61748b26c88ac6e2ba
Carlos Eduardo <cadubcastro@gmail.com>
2024-08-16 22:54:59 -0300

    Finalizando script para o desafio proposto

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,12 @@
+
+public class ParametrosInvalidosException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException() {
+		System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+	}
+}
